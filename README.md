9198b74b073c4680ecd128327c1206bbdbdf6524
Edwin Obando <webeao@gmail.com>
2020-04-02 21:42:53 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Aplicacion de comandos
+
+Este es el ejercicio del curso
+
+Recuerden instalas los paquetes de node asi:
+
+npm install
---
+++ app.js
@@ -0,0 +1,46 @@
+const argv = require('./config/yargs').argv;
+const porHacer = require('./tareas-por-hacer/tareas-por-hacer');
+const colors = require('colors');
+
+console.log(argv);
+
+let comando = argv._[0];
+
+
+switch (comando) {
+    case 'crear':
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
+        break;
+    case 'listar':
+        let listado = porHacer.getListado();
+
+        for (let tarea of listado) {
+            console.log("=================");
+            console.log(tarea.descripcion);
+            console.log('estado', tarea.completado);
+            console.log("=================");
+        }
+
+        break;
+    case 'actualizar':
+        let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
+        console.log(actualizado);
+        break;
+
+    case 'borrar':
+        let borrado = porHacer.borrar(argv.descripcion);
+        console.log(borrado);
+        break;
+
+
+
+    default:
+        console.log('comando no es reconocido');
+
+}
+
+
+
+
+
